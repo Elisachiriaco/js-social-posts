57f6930de928e03d75cfeb4c2aea2addb5b188c1
elisachiriacogit config --global user.email <elisachiriaco22@outlook.it>
2022-04-19 11:20:25 +0200

    esercizio con bonus completato

--- js/main.js
+++ js/main.js
@@ -72,58 +72,85 @@ const posts = [
     }
 ];
 
+const userLikes = [];
 
-// Creo funzione per stampare le card
-let container = document.getElementById("container");
+function getInitials(name) {
+    const nameParts = name.split(' ');
+    const initials = nameParts[0].charAt(0) + nameParts[1].charAt(0);
+    return initials.toUpperCase();
+}
 
-function stampaCard(){
-    posts.forEach((post)=>{
-        container.innerHTML += `
-            <div class="post">
-                <div class="post__header">
-                    <div class="post-meta">                    
-                        <div class="post-meta__icon">
-                            <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">                    
-                        </div>
-                        <div class="post-meta__data">
-                            <div class="post-meta__author">${post.author.name}</div>
-                            <div class="post-meta__time">${post.created}</div>
-                        </div>                    
-                    </div>
+// Disegno gli item
+function drawGridItem(post){
+    const template = `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                ${post.author.image ? `<img class="profile-pic" src="${post.author.image}" alt="${post.author.name}"> ` : `<div class="profile-pic-default"><span>${getInitials(post.author.name)}</span></div>`}                   
                 </div>
-                <div class="post__text">${post.content}</div>
-                <div class="post__image">
-                    <img src="${post.media}" alt="img">
-                </div>
-                <div class="post__footer">
-                    <div class="likes js-likes">
-                        <div class="likes__cta">
-                            <a class="like-button  js-like-button" href="#" data-postid="${post.id}">
-                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                                <span class="like-button__label">Mi Piace</span>
-                            </a>
-                        </div>
-                        <div class="likes__counter">
-                            Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
-                        </div>
-                    </div> 
-                </div>            
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${post.author.name}</div>
+                    <div class="post-meta__time">${new Date(post.created).toLocaleDateString()}</div>
+                </div>                    
             </div>
-        `;
-        // console.log(post);
+        </div>
+        <div class="post__text">${post.content}</div>
+        <div class="post__image">
+            <img src="${post.media}" alt="img">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="${post.id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-${post.id}" class="js-likes-counter">${post.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+`;
+    return template
+}
+
+// disegno la griglia
+function stampaCard(){
+    let container = document.getElementById("container");
+    let html = '';
+    posts.forEach((post)=>{
+        const card = drawGridItem(post)
+        html += card;
     })
+    container.innerHTML += html;
 };
-stampaCard();
 
-// Creo funzione per cambiare colore al tasto like e incrementare like
-const like = document.querySelectorAll('.like-button')
+function setLikes(check,postid){
+    const counter = document.getElementById(`like-counter-${postid}`);
+            let likeValue = parseInt(counter.innerText);
+    if(check){
+        userLikes.push(postid);
+        counter.innerText = likeValue + 1;
+    } else{
+        const index = userLikes.indexOf(postid);
+        if(index !== - 1)  userLikes.splice(index,1);
+        counter.innerText = likeValue - 1;
+    }
+}
 
-function miPiace(){
-    like.forEach((likes)=>{
-        likes.addEventListener("click", function(){
-            likes.classList.add("like-button--liked");
+function attachEvent(){
+    const likeButtons = document.querySelectorAll('[data-postid]');
+    likeButtons.forEach((button) => {
+        button.addEventListener('click',function(evento){
+            evento.preventDefault();
+            this.classList.toggle("like-button--liked");
+            const check = this.classList.contains("like-button--liked");
+            setLikes(check,this.dataset.postid)
         })
     })
-};
-miPiace();
-
+}
+stampaCard();
+attachEvent();
